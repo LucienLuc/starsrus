2a4fd6ed052beffbdc9e4c9ed0f20a0b2328a87d
LucienLuc <lucienluc@umail.ucsb.edu>
2021-05-15 17:14:34 -0700

    Starter files

---
+++ java/connect/net/source/Main.java
@@ -0,0 +1,60 @@
+package net.source;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class Main {
+     /**
+     * Connect to a sample database
+     */
+    public static String url = "jdbc:sqlite:/Users/lucienluc/Downloads/Spring 2021/CMPSC174a/Project/db/chinook.db";
+
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            // String url = "jdbc:sqlite:/Users/lucienluc/Downloads/Spring 2021/CMPSC174a/Project/db/chinook.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public static void createTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+            System.out.println("Created table");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        connect();
+        createTable();
+    }
+}
